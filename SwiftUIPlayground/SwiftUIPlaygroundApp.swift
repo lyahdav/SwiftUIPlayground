2b5d56c3374c6d506d7358b6553c70f6b3abe977
Liron Yahdav <lyahdav@users.noreply.github.com>
2025-09-30 09:06:25 -0700

    Initial Commit

---
+++ SwiftUIPlayground/SwiftUIPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPlaygroundApp.swift
+//  SwiftUIPlayground
+//
+//  Created by Liron Yahdav on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
